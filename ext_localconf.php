491fe05cd1c1babd45a0e65cf3a6ed36ee5a6f89
Thomas Schenk <Thomas.Schenk@aoe.com>
2019-04-29 13:10:19 +0200

    [TASK] converts old class strings to new classes in hook conf

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,28 +10,26 @@ const REAL_URL_CACHE_ENCODE = 'realurl_encode';
 if (TYPO3_MODE === 'FE') {
     //hook to force regeneration if crawler is active:
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['insertPageIncache']['tx_realurl'] =
-        'EXT:realurl/class.tx_realurl_crawler.php:tx_realurl_crawler';
+        \AOE\Realurl\Crawler::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['headerNoCache']['tx_realurl'] =
-        'EXT:realurl/class.tx_realurl_crawler.php:tx_realurl_crawler->headerNoCache';
+        \AOE\Realurl\Crawler::class . '->headerNoCache';
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearPageCacheEval']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl.php:&tx_realurl->clearPageCacheMgm';
+    \AOE\Realurl\Realurl::class . '->clearPageCacheMgm';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl_tcemain.php:&tx_realurl_tcemain';
+    \AOE\Realurl\Tcemain::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl_tcemain.php:&tx_realurl_tcemain';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamap_preProcessFieldArray']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl_processdatamap.php:&tx_realurl_processdatamap';
+    \AOE\Realurl\Tcemain::class;
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['linkData-PostProc']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl.php:&tx_realurl->encodeSpURL';
+    \AOE\Realurl\Realurl::class . '->encodeSpURL';
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl.php:&tx_realurl->encodeSpURL_urlPrepend';
+    \AOE\Realurl\Realurl::class . '->encodeSpURL_urlPrepend';
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc']['tx_realurl'] =
-    'EXT:realurl/class.tx_realurl.php:&tx_realurl->decodeSpURL';
+    \AOE\Realurl\Realurl::class . '->decodeSpURL';
 
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_realurl_pathsegment,tx_realurl_exclude,tx_realurl_pathoverride';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_realurl_pathsegment,tx_realurl_exclude,tx_realurl_pathoverride';
