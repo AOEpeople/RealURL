db6a5c8b10ba710877ac2fb9aff1faad43851812
Thomas Schenk <Thomas.Schenk@aoe.com>
2019-05-02 17:01:07 +0200

    [TASK] corrects requirements and documentation

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.12.8.13.AOE',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.99.99',
+            'php' => '7.0.0-7.2.99',
             'typo3' => '8.7.0-8.7.99',
         ],
         'conflicts' => [
