a41f19a072fe94c2b8e415c6415f1536041a3d43
Felix Semmler <felix.semmler@aoe.com>
2020-03-02 13:11:28 +0100

    [CLEANUP] Remove useless function decodeSpURL_fixMagicQuotes (was not executed since PHP 5.4)
    See: https://www.php.net/manual/en/function.get-magic-quotes-gpc.php

--- Classes/Realurl.php
+++ Classes/Realurl.php
@@ -1421,25 +1421,11 @@ class Realurl
             if ($GET_string) {
                 $GET_VARS = false;
                 parse_str($GET_string, $GET_VARS);
-                $this->decodeSpURL_fixMagicQuotes($GET_VARS);
                 $this->decodeSpURL_fixBrackets($GET_VARS);
                 return $GET_VARS;
             }
         }
         return null;
-    }
-
-    /**
-     * Fix for the magic_quotes_gpc. See http://bugs.typo3.org/view.php?id=18133
-     *
-     * @param mixed $array
-     * @return void
-     */
-    protected function decodeSpURL_fixMagicQuotes(&$array)
-    {
-        if (is_array($array)) {
-            self::stripSlashesOnArray($array);
-        }
     }
 
     /**
